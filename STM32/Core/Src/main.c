6ea23f0c2e150299637943cfa6229287f29b4fa8
huyne265 <huy.ledang265@hcmut.edu.vn>
2024-09-15 22:49:09 +0700

    init Ex7

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -185,7 +185,7 @@ void initState(){
 }
 	const int MAX_LED = 4;
 	int index_led = 0;
-	int led_buffer[4] = {1,2,3,4};
+	int led_buffer[4];
 void update7SEG(int index){
 		switch(index){
 			case 0:
@@ -252,13 +252,20 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int hour = 15, minute = 8 , second = 50;
-  setTimer(0, 1000); // 7SEG
+  const int CYCLE = 1000;
+  int hour = 23, minute = 59 , second = 50;
+  setTimer(0, CYCLE/4); // 7SEG
   setTimer(1, 10); // RED_LED
   setTimer(2, 1000); // DOT
   setTimer(3, 1000); // Clock
   initState();
   while(1){
+	  if(timer_flag[0] == 1){
+		  setTimer(0, CYCLE/4);
+		  if(index_led >= 4) index_led = 0;
+		  update7SEG(index_led++);
+	  }
+
 	  if(timer_flag[1] == 1){
 		setTimer(1, 500);
 		HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
@@ -406,16 +413,7 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-const int CYCLE = 1000;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	if(index_led >= MAX_LED) index_led = 0;
-	if(timer_flag[0] == 1){
-		setTimer(0, CYCLE);
-	}
-	if(timer_counter[0] == CYCLE/10) update7SEG(index_led++);
-	else if(timer_counter[0] == CYCLE*3/4 /10) update7SEG(index_led++);
-	else if(timer_counter[0] == CYCLE/2 /10) update7SEG(index_led++);
-	else if(timer_counter[0] == CYCLE/4 /10) update7SEG(index_led++);
 	timerRun();
 }
 /* USER CODE END 4 */
